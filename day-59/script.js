4c5fc5f931f4cc3e86ba33c64a2687f45b7bc291
datimikhelashvili <146359873+datimikhelashvili@users.noreply.github.com>
2024-04-10 18:22:52 +0400

    Add files via upload

---
+++ day-59/script.js
@@ -0,0 +1,47 @@
+function mathoperation1(x , y) {
+    console.log(20 + 10)
+}
+mathoperation1()
+
+function mathoperation2(x , y) {
+    console.log(20 - 10)
+}
+mathoperation2()
+
+function mathoperation3(x , y) {
+    console.log(20 * 10)
+}
+mathoperation3()
+
+function mathoperation4(x , y) {
+    console.log(20 / 10)
+}
+mathoperation4()
+
+function square(x, y) {
+    console.log(12 * 5)
+}
+square()
+
+function goaBest() {
+    console.log("goa best")
+    console.log("goa best")
+    console.log("goa best")
+    console.log("goa best")
+    console.log("goa best")
+    console.log("goa best")
+    console.log("goa best")
+    console.log("goa best")
+    console.log("goa best")
+    console.log("goa best")
+}
+goaBest()
+
+function perimeter(x , y , z , v) {
+    console.log(20 + 20 + 10 + 10)
+}
+perimeter()
+
+function person() {
+    console.log("family members: lika mikhelashvili , murtaz mikhelashvili , tako mikhelashvili , nona janashvili , ilo mikhelashvili lia ajiashvili. they are 21, 52 , 19 , 42 , 83 , 76 and they live at nadzaladevi")
+}
