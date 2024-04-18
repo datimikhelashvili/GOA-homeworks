b153bd9eb774727369e2cd0a40c6a34089038212
datimikhelashvili <146359873+datimikhelashvili@users.noreply.github.com>
2024-04-18 21:24:21 +0400

    Add files via upload

---
+++ day-61/script.js
@@ -0,0 +1,56 @@
+// 20 shedarebis magaliti
+
+console.log(5 > 1)
+console.log(4 < 5)
+console.log(48 < 12)
+console.log(52 > 41)
+console.log(41 > 64)
+console.log(421 < 1)
+console.log(9999 < 1)
+console.log(12 > 43)
+console.log(41 > 42)
+console.log(124 > 41)
+console.log(123 > 142)
+console.log(124 == 124)
+console.log(527 > 52)
+console.log(523 > 412)
+console.log(12 > 5342)
+console.log(6342 < 2341)
+console.log(4214 > 321)
+console.log(2342 > 532)
+console.log(5327 > 32)
+console.log(1 == 1)
+
+// 10 true magaliti
+
+console.log(69 == 69)
+console.log(45 < 412)
+console.log(6387 > 1)
+console.log(412 > 411)
+console.log(56 == 56)
+console.log(60 > 53)
+console.log(693 > 421)
+console.log(523 > 43)
+console.log(45 < 4123)
+console.log(567 > 523)
+
+// 10 false magaliti
+
+console.log(4321 == 5423)
+console.log(435 > 1)
+console.log(5643 == 21)
+console.log(432 == 234)
+console.log(4321 >14214)
+console.log(421 < 421)
+console.log(45324 == 23)
+console.log(532 == 4321)
+console.log(512 > 3244)
+console.log(5326 > 52345)
+
+// shedareba funqciit
+
+function mathOperation(x,y) {
+    console.log(20 < 21)
+}
+
+mathOperation()
