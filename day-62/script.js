403fcef646d21b0b8bc3c4ccbabfa4a08687e090
datimikhelashvili <146359873+datimikhelashvili@users.noreply.github.com>
2024-04-29 21:23:56 +0400

    Add files via upload

---
+++ day-62/script.js
@@ -0,0 +1,21 @@
+if (20 < 40) {
+    console.log("you are right")
+}
+else (20 < 40) ;{
+    console.log("you are wrong")
+}
+
+if (20 > 40) {
+    console.log("you are right")
+}
+
+else (20 > 40) ;{
+    console.log("you are wrong")
+}
+
+if (20 == 40) {
+    console.log("you are right")
+}
+else (20 == 40) ;{
+    console.log("you are wrong")
+}
---
+++ day-63/script.js
@@ -0,0 +1,8 @@
+let user = {
+    name: "dati",
+    lastname: "mikhelashvili",
+    adress: "nadzaladevi",
+    password: "421421"
+}
+
+console.log("ჩემი სახელია" + user.name + "ჩემი გვარია" + user.lastname + "ჩემი მისამართია " + user.adress + "ჩემი პაროლია" + user.password)
